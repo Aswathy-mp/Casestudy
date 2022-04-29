d4dcb9899d4ee882769285597feec8d1330c4f09
Aswathy-mp <aswathymp24@gmail.com>
2022-04-29 11:13:25 +0530

    commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 
 let username=document.getElementById("username");
+let usermsg=document.getElementById('usermsg')
 let pwd=document.getElementById("pwd");
+let pwdmsg=document.getElementById('pwdmsg');
 username.addEventListener("input",userValidate);
 pwd.addEventListener("input",pwdValidate);
+
+// alert('Username : admin and Password : 12345')
+
 function userValidate(){
+    usermsg.innerHTML='username: admin'
+    usermsg.style.color='green'
     if(username.value=="admin"){
+        
         console.log("username");
         return true;
     }
@@ -13,6 +21,8 @@ function userValidate(){
     }
 }
 function pwdValidate(){
+    pwdmsg.innerHTML='password: 12345'
+    pwdmsg.style.color='green'
     if(pwd.value=="12345"){
         return true;
     }
